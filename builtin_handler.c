c86c1bb9540211f1a972ff752fcdabee2d90148a
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-05-23 19:08:11 -0700

    simple shell

---
+++ builtin_handler.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * handle_exit - handles exit command.
+ * @m: a struct.
+ * Return: void.
+ */
+
+void handle_exit(struct memory *m)
+{
+	free_array_of_strings(m->agv);
+	free(m->command);
+	exit(0);
+}
+
+/**
+ * handle_env - handles the environments.
+ * @m: a struct.
+ * Return: void.
+ */
+
+void handle_env(struct memory *m)
+{
+	char **env;
+
+	(void)m;
+	env = environ;
+	while (*env)
+	{
+		puts(*env);
+		env++;
+	}
+}
---
+++ shell.h
@@ -0,0 +1,199 @@
+#ifndef SHELL
+#define SHELL
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stddef.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+extern char **environ;
+
+/**
+ * struct DynamicString - contains dynamic strings and functions
+ * @str:pointer to character
+ * @len:size_t variable
+ * @capacity:size_t variable
+ * @append_char: a function that appends character
+ * @append_string: a function that appends string
+ * @free: a function that sets the string to free
+ * @clear: a function that clears the memory.
+ */
+typedef struct DynamicString
+{
+	char *str;
+	size_t len;
+	size_t capacity;
+
+	void (*append_char)(struct DynamicString *, char);
+	void (*append_string)(struct DynamicString *, const char*);
+	void (*free)(struct DynamicString *);
+	void (*clear)(struct DynamicString *);
+} DynamicString;
+
+/**
+ * struct memory - a struct representing a change in memory
+ *
+ * @command: a string representing the name of the command
+ * @agv: a string pointer represents the arguments
+ * @command_number: an integer
+ * @program_args: a string for arrguments
+ * @program_arg_count: an integer that counts the args.
+ * struct built_in - a struct representing a built_in function
+ * @builtIn: a built_in struct
+ * @current_process_id: represents the pid
+ * @last_exit_code: represents the exit code.
+ */
+typedef struct memory
+{
+	char *command;
+	char **agv;
+	int command_number;
+	char **program_args;
+	int program_arg_count;
+	struct built_in *builtIn;
+	pid_t current_process_id;
+	int last_exit_code;
+} memory;
+
+/**
+ * struct built_in - a struct representing a built-in command
+ *
+ * @command: a string representing the name of the command
+ * @handler: a function pointer to the handler function for the command
+ */
+
+typedef struct built_in
+{
+	char *command;
+	void (*handler)(struct memory *m);
+} built_in;
+
+
+/**
+ * struct ArgsHandler - a struct representing arguments handlers
+ * @name: name of the argument
+ * struct handler - a struct representing arguments
+ * @handler: a function pointer to the handler function for the     command
+ */
+typedef struct ArgsHandler
+{
+	char *name;
+	void (*handler)(struct memory *m, int i);
+} ArgsHandler;
+
+
+
+/**shell_helpers**/
+/* count_of_tok - counts the number of tokens */
+int count_of_tok(char *command, char *del);
+/* arr_of_tokens - copies the tokens into array of strings */
+char **arr_of_tokens(char *command, char *del);
+/* string_array_cp - copies the array of string  */
+void string_array_cp(char ***des, char **src);
+/* converts int to string */
+void int_to_string(int number, char *str);
+
+
+/**real_string**/
+/* appends the string */
+void append_string(DynamicString *ds, const char *str);
+/* appends the character */
+void append_char(DynamicString *ds, char ch);
+/* free the dynamic variable */
+void free_ds(DynamicString *ds);
+/* gives values to the struct */
+void clear(DynamicString *ds);
+/* initialize the dynamic string */
+void init_dynamic_string(DynamicString *ds, char *init);
+
+
+
+/**debugger**/
+/* Prints a string */
+void print_string(char *s);
+/* Prints array of strings */
+void print_array_of_strings(char **s);
+
+
+
+/**string**/
+/* returns a written string */
+int write_charV(int fd, const char *buffer, size_t size);
+/* free the array of string */
+void free_array_of_strings(char **a);
+/* concatinate 2 strings */
+char *concatenate_string(char *s1, char *s2);
+
+
+
+/**env**/
+/* Divides the string into tokens */
+char *_getenv(char *name);
+
+
+
+/**shell2**/
+/* Fork and execute the command */
+int execute_commandV(memory *m);
+/* Handles the built_in commands */
+int handle_built_in(struct memory *m);
+/* Handles args */
+int handle_args(memory *m);
+
+
+
+/**shell**/
+/* Control the (ctrl+c) */
+void sigintHandler(int signal);
+/* check_command_exit - checks the command */
+int check_command_exit(char **args);
+/* compares 2 strings together */
+int my_strcmp2(const char *s1, const char *s2);
+/* check_built_ins - checks the built_in commands */
+int check_built_ins(char **args);
+/* the main function */
+/*int main(int argc, char **argv)*/
+
+
+
+/**string_helpers**/
+/* measures the lenght of the string */
+size_t _strlen(char *s);
+/* copies the string from one pointer to another */
+char *_strcpy(char *m, char *n);
+/* compares the strings */
+int _strcmp(char *s, char *x);
+/* copies the string from one pointer to another */
+char *_strncpy_size(char *destination, const char *source, size_t num);
+
+
+/**Memory**/
+/* copies a block of memory from one place to another */
+void _memcpy(void *destination, void *source, size_t number_of_bytes);
+/* re-allocate the memory */
+void *_memset(void *ptr, int value, size_t num);
+/* fill a block of memory with a specific value */
+void *_realloc(void *ptr, size_t size, size_t old_size);
+
+
+/**handle_args**/
+/* handle the hash for non interactive mode */
+void handle_hash(struct memory *m, int i);
+/* handles the double dollar sign */
+void handle_double_dollar(struct memory *m, int i);
+/* handles the double mark */
+void handle_double_mark(struct memory *m, int i);
+
+
+/**handle_build_in**/
+/* handles exit command */
+void handle_exit(struct memory *m);
+/* handles the environments */
+void handle_env(struct memory *m);
+#endif
+
